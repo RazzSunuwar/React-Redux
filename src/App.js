8473d54029d7422a5300bb3f24468215b4a8b545
raysunwar <ray@whitehatagency.com.au>
2020-04-03 13:31:31 +0545

    Methods as props

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
 import EventBind from './components/EventBind';
+import ParentComponet from './components/ParentComponent';
 
 class App extends Component {
   render() {
@@ -39,7 +40,9 @@ class App extends Component {
 
         {/* <ClassClick /> */}
 
-        <EventBind />
+        {/* <EventBind /> */}
+
+        <ParentComponet />
 
 
 
