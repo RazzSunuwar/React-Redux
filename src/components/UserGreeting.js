d10cd8c6d1ad6ad7e1ea6eeaac5d180ac45da6a6
raysunwar <ray@whitehatagency.com.au>
2020-04-03 14:31:57 +0545

    Conditional Rendering

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -6,23 +6,32 @@ class UserGreeting extends Component {
         super(props)
     
         this.state = {
-             isLoggedIn: false
+             isLoggedIn: true
         }
     };
     
     render() {
-        if(this.state.isLoggedIn){
-            return <div>WEL COME MAN!</div>
-        } else {
-            return <div>WEL COME GUEST!</div>
-        }
+        // if(this.state.isLoggedIn){
+        //     return <div>WEL COME MAN!</div>
+        // } else {
+        //     return <div>WEL COME GUEST!</div>
+        // }
         // return (
         //     <div>
         //         <div>WEL COME MAN!</div>
         //         <div>WEL COME GUEST!</div>
         //     </div>
         // )
-    }
+
+
+        let message;
+        if(this.state.isLoggedIn){
+            message = <div>WEL COME MAN!</div>
+        } else {
+            message = <div>WEL COME GUEST!</div>
+        }
+        return<div>{message}</div>
+    };
 };
 
 export default UserGreeting;
