13158be9c615f5fe0681ffa6b43dbb816d5d883d
RazzSunuwar <razzkoinch5@gmail.com>
2020-04-06 21:07:40 +0545

    memo

--- src/ParentComp.js
+++ src/ParentComp.js
@@ -1,6 +1,7 @@
 import React, { Component, PureComponent } from 'react';
 import RegularComp from './RegularComp';
 import PureComp from './PureComp';
+import MemoComponent from './components/MemoComponent';
 
 class ParentComp extends Component {
     constructor(props) {
@@ -24,8 +25,10 @@ class ParentComp extends Component {
         return (
             <div>
                Parent Component
-               <RegularComp name={this.state.name} />
-               <PureComp name={this.state.name} />
+               {/* <RegularComp name={this.state.name} /> */}
+               {/* <PureComp name={this.state.name} /> */}
+
+               <MemoComponent name ={this.state.name} />
             </div>
         );
     };
