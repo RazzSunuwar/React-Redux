c5f7849c3ea72324f4c98c892247b14ad28d35ef
RazzSunuwar <razzkoinch5@gmail.com>
2020-04-07 19:12:49 +0545

    Contex

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import ComponentF from './ComponentF';
+import UserContext from './UserContext';
 
 class ComponentE extends Component {
+    static contextType = UserContext
     render() {
         return (
             <div>
-               <ComponentF /> 
-                
+                Component E Component {this.context}
+                <ComponentF />
             </div>
         )
     }
 }
 
+// ComponentE.contextType = UserContext     Same as static
+
 export default ComponentE
+
+
+// Limitation
+// Only work on the class component
+// Only suscribe to a single context using contexType
