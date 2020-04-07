38d26541618bc15fb4c088ab68bbce7b9deea597
raysunwar <ray@whitehatagency.com.au>
2020-04-07 12:30:46 +0545

    updatedComponent HOC

--- src/components/ClickCounter.js
+++ src/components/ClickCounter.js
@@ -2,24 +2,14 @@ import React, { Component } from 'react';
 import updatedComponent from './withCounter'
 
 export class ClickCounter extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            count: 0
-        };
-    };
 
-    incrementCount = () => {
-        this.setState(prevState => {
-            return { count: prevState.count + 1}
-        });
-    };
-    
     render() {
-        const { count } = this.state    
-        return <button onClick={this.incrementCount}>
-        {this.props.name} Click {count} Times</button>
+        const { count, incrementCount } = this.props   
+        return (
+            <button onClick={incrementCount}>
+                Click {count} Times
+            </button>
+        )
     };
 };
 
